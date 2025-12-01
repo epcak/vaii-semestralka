9c70d2073494d74b6ff74966f4d9bfa889fc07bd
epcak <epcaksk@gmail.com>
2025-12-01 14:21:43 +0100

    Prieriedenie pohladov a s nimi spojene subory

--- App/Configuration.php
+++ App/Configuration.php
@@ -19,7 +19,7 @@ class Configuration
     /**
      * Application name.
      */
-    public const APP_NAME = 'Vaííčko MVC FW';
+    public const APP_NAME = 'Zaujmovy web';
 
     /**
      * Version of the framework.
